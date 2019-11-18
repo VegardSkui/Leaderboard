fc076aca413e52837f30ba4018a6f51b11b9cf94
Vegard Skui <me@vegardskui.com>
2019-11-18 19:29:42 +0100

    don’t include localhost check in production

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import RevolvePingPong from "./RevolvePingPong"
 
 const db = firebase.firestore()
 
-// Connect to the emulator if on localhost
-if (location.hostname === "localhost") {
-    db.settings({
-        host: "localhost:8080",
-        ssl: false
-    })
+// Connect to the emulator if not in production and on localhost
+if (process.env.NODE_ENV !== "production") {
+    if (location.hostname === "localhost") {
+        db.settings({
+            host: "localhost:8080",
+            ssl: false
+        })
+    }
 }
 
 // Kickoff
